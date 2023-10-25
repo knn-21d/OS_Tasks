ef04d908bc7e53bbcf7c2494e2960708ab6fe63c
knn-21d <109787964+knn-21d@users.noreply.github.com>
2023-10-26 02:02:29 +0300

    Update 1_1.cpp

--- Task_1/1_1.cpp
+++ Task_1/1_1.cpp
@@ -7,22 +7,22 @@ using namespace std;
 int main(int argc, char* argv[]) {
     int n = stoi(argv[1]);
 
-    cout << "main process started... PID = " << to_string(getpid()) << endl;
+    cout << "main process started... PID = " << getpid() << endl;
 
     for (int i = 0; i < n; i++) {
         pid_t pid = fork();
 
         if (pid == 0) {
-            cout << "child started... PID = " << to_string(getpid()) << ", PPID = " <<
-            to_string(getppid()) << " (i = " << to_string(i) << ")" << endl;
+            cout << "child started... PID = " << getpid() << ", PPID = " <<
+            getppid() << " (i = " << i << ")" << endl;
             sleep(5);
-            cout << to_string(i) << " child finished... PID = " << to_string(getpid()) <<
-            ", PPID = " << to_string(getppid()) << endl;
+            cout << i << " child finished... PID = " << getpid() <<
+            ", PPID = " << getppid() << endl;
             return 0;
         }
     }
 
-    cout << "main process finished... PID = " << to_string(getpid()) << endl;
+    cout << "main process finished... PID = " << getpid() << endl;
 
     return 0;
 }
